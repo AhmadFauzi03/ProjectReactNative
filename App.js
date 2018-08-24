c461e53ab704c1b5568e34d2ddd6f11baa8522ec
Ahmad Fauzi <ahmadfauzi282002@gmail.com>
2018-08-24 16:32:35 +0700

    second commit

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, Image} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -21,11 +21,12 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome!</Text>
+        <Text style={styles.welcome}>Welcome!!</Text>
         <View>
         <Text style={styles.instructions}>Nama  : Ahmad Fauzi Budiyanto</Text>
         <Text style={styles.instructions}>Kelas   : XI RPL 1</Text>
         <Text style={styles.instructions}>Absen  : 03</Text>
+        <Image source={require('./fotoku.jpg')} style={{width: 350, height: 250}} />
         </View>
       </View>
     );
